c7bf5817fdf17ccea3bffd0650875fdbb192fe62
fadilaziz <fadilaziz275@gmail.com>
2025-09-05 07:50:34 +0700

    Belajar bentar

--- DOM/DOM Manipulation part 4/study kasus 1.1/script.js
+++ DOM/DOM Manipulation part 4/study kasus 1.1/script.js
@@ -21,3 +21,25 @@ tombolAcakWarna.addEventListener('click', function() {
     document.body.style.backgroundColor = ('rgb(' + r + ',' + g + ',' + b + ')');
 })
 
+const kotakMerah = document.querySelector('input[name="range-red"]');
+const indikatorAngkaMerah = document.getElementsByClassName('indikator-red')[0].getElementsByTagName('p')[0];
+const kotakHijau = document.querySelector('input[name="range-green"]');
+const indikatorAngkaHijau = document.getElementsByClassName('indikator-green')[0].getElementsByTagName('p')[0];
+const kotakBiru = document.querySelector('input[name="range-blue"]');
+const indikatorAngkaBiru = document.getElementsByClassName('indikator-blue')[0].getElementsByTagName('p')[0];
+kotakMerah.addEventListener('input', function() {
+    indikatorAngkaMerah.innerText = kotakMerah.value;
+} )
+kotakHijau.addEventListener('input', function() {
+    indikatorAngkaHijau.innerText = kotakHijau.value;
+} )
+kotakBiru.addEventListener('input', function() {
+    indikatorAngkaBiru.innerText = kotakBiru.value;
+} )
+
+// const tombolWarna = document.getElementById('tombolWarna');
+
+tombolWarna.addEventListener('change', function() {
+    const body = document.body;
+    body.style.backgroundColor = 'rgb(' + kotakMerah.value + ',' + kotakHijau.value + ',' + kotakBiru.value + ')';
+} )
