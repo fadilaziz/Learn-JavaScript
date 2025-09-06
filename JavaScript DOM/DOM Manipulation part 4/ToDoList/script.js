4d01f4173f991bf1b04030cfae3380d633d80b7b
fadilaziz <fadilaziz275@gmail.com>
2025-09-06 20:43:18 +0700

    update Ripository

---
+++ JavaScript DOM/DOM Manipulation part 4/ToDoList/script.js
@@ -0,0 +1,9 @@
+const inputTask = document.getElementById('task').value;
+const tombolTask = document.getElementById('addTask');
+const task = document.createElement('li');
+
+inputTask.app
+
+tombolTask.addEventListener('click', function() {
+
+})
---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+//Type Data
+let numberdata = -1.4;
+let stringData = "sadfahduaewdew";
+let arrayData = [1,2,3, ' ini string'];
+let booleanData = true; //false
+let objectdata = {
+    name : "udin",
+    age : 20,
+    address : "indonesia"
+}
+
+console.log(1 + "1");
