48b3c9760b942e148f49c15ceea71bf7309132ce
fadilaziz <fadilaziz275@gmail.com>
2025-09-11 14:36:49 +0700

    Hapus Sebagian

--- JavaScript DOM/DOM Manipulation part 3/ToDoList/script.js
+++
@@ -1,9 +0,0 @@
-const inputTugas = document.getElementById('taskInput').value;
-const tugasBaru = document.createElement('li');
-const tombolTambah = document.getElementById('addTaskButton');
-
-tombolTambah.addEventListener('click', function() {
-    const task = tugasBaru.cloneNode();
-    task.textContent = inputTugas;
-    document.getElementById('taskList').appendChild(task);
-})
